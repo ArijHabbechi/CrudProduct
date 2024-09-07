58167d53588818055ff9fda4ba78e04db9cc47d8
root <arij.habachi@insat.ucar.tn>
2024-09-07 13:32:06 -0700

    Fixed trivy scan script

--- trivy-docker-scan.sh
+++ trivy-docker-scan.sh
@@ -1,31 +1,25 @@
 #!/bin/bash
 
 # Variables
-DOCKERFILE="Spring/Dockerfile"   
-TRIVY_REPORT_HTML="trivy-report.html"
-TRIVY_REPORT_XML="trivy-report.xml"
+DOCKERFILE="Spring/Dockerfile"
 
 # Extract image names from the Dockerfile
 IMAGES=$(grep "^FROM" "$DOCKERFILE" | awk '{print $2}')
 
-
-
 # Run Trivy scan for each image and generate reports
 for IMAGE in $IMAGES; do
     echo "Scanning image: $IMAGE"
-    
+
     # Run Trivy for each image and generate an HTML report
     docker run --rm \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v $(pwd):/root/.cache/ \
-    aquasec/trivy:latest image --format html -o "$IMAGE-$TRIVY_REPORT_HTML" "$IMAGE"
-    
-    # Generate XML report (JUnit format)
-    docker run --rm \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v $(pwd):/root/.cache/ \
-    aquasec/trivy:latest image --format template --template "@contrib/junit.tpl" -o "$IMAGE-$TRIVY_REPORT_XML" "$IMAGE"
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -v $(pwd):/root/.cache/ \
+        -v $(pwd)/html.tpl:/html.tpl \
+        -v $(pwd):/output \
+        aquasec/trivy:0.37.3 image --timeout 15m --format template --template "@/html.tpl" -o /output/${IMAGE}-trivy-report.html $IMAGE
+
+    echo "Trivy scan for $IMAGE completed."
 done
 
-echo "Trivy scans completed."
+echo "All Trivy scans completed."
 
