f732040f2c99100590039ecec1c806749e73cad5
ArijHabbechi <arij.habachi@insat.ucar.tn>
2024-09-08 16:50:55 -0700

    secrets updated

--- update-mysql-secret.sh
+++ update-mysql-secret.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd Spring
 # Load .env file
 set -o allexport
 source .env
@@ -11,6 +12,8 @@ MYSQL_DATABASE_BASE64=$(echo -n "$MYSQL_DATABASE" | base64)
 MYSQL_USER_BASE64=$(echo -n "$MYSQL_USER" | base64)
 MYSQL_PASSWORD_BASE64=$(echo -n "$MYSQL_PASSWORD" | base64)
 
+
+cd ..
 # Update the existing mysql-secret.yaml file
 sed -i "s|\(MYSQL_ROOT_PASSWORD:\s*\).*|\1$MYSQL_ROOT_PASSWORD_BASE64|" mysql-secret.yml
 sed -i "s|\(MYSQL_DATABASE:\s*\).*|\1$MYSQL_DATABASE_BASE64|" mysql-secret.yml
